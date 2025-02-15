b37afc59163f8c3c26625c625736b443a621ee5b
Lucas <contato.developerlucas@gmail.com>
2025-02-14 22:20:55 -0300

    fix: deploy

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,33 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,POST,PUT,DELETE,OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type, Authorization',
+          },
+        ],
+      },
+    ]
+  },
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'qcqxhnibanpmhlajvhos.supabase.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'sandbox.api.pagseguro.com',
       },
     ],
   },
